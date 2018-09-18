a8826ea769b9f984e0a4dba2243b0e5aec98292f
iwedding <>
2018-09-18 15:05:39 +0900

    first commit

---
+++ app/src/main/java/com/tistory/black_jin0427/myrecyclerview/MainActivity.java
@@ -0,0 +1,37 @@
+package com.tistory.black_jin0427.myrecyclerview;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.widget.LinearLayout;
+
+import com.tistory.black_jin0427.myrecyclerview.adapter.MovieAdapter;
+import com.tistory.black_jin0427.myrecyclerview.data.SampleData;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MovieAdapter adapter = new MovieAdapter();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //recycleView 초기화
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        //세로 레이아웃
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
+
+        //아이템 로드
+        adapter.setItems(new SampleData().getItems());
+
+    }
+}
